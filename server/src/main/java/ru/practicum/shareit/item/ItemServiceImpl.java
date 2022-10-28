a1b1a6b6f11eea4604acee89bbfa283305f250d5
Aleksey Kozlovskiy <kyzinka@yandex.ru>
2022-10-28 18:29:03 +0300

    TZ 16 add gateway/server

--- server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -22,7 +22,6 @@ import ru.practicum.shareit.util.UserValidation;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
